3b9a930b218c6e9641ff16987032293b3dd30fe7
ludnas <ksandul@gmail.com>
2020-03-08 16:22:02 -0400

    CPU done

--- metrics.py
+++ metrics.py
@@ -3,15 +3,15 @@ import psutil
 import os
 
 result = {}
-print(sys.argv[0])
+# print(psutil.cpu_times_percent())
 if sys.argv[1] == "mem":
-    print(psutil.cpu_percent())
+    print(psutil.virtual_memory())
 elif  sys.argv[1] == "-cpu":
-    cpuStat =  psutil.cpu_times_percent()
+    cpuStat =  psutil.cpu_times_percent(interval=1)
     result["system.cpu.idle"] = cpuStat[3]
     result["system.cpu.user"] =  cpuStat[0]
     result["system.cpu.guest"] =  cpuStat[8]
-    result["system.cpu.iowait"] =  cpuStat[3]
+    result["system.cpu.iowait"] =  cpuStat[4]
     result["system.cpu.stolen"] =  cpuStat[6]
     result["system.cpu.system"] =  cpuStat[2]
     for k, v in result.items():
